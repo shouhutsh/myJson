53bf4c4c66c2bd6423ba2851c8a950c78d5ef78b
shouhutsh <shouhutsh@gmail.com>
2015-04-07 10:41:43 +0800

    对JsonEntity的结构进行重构，我感觉这样更清晰一些

--- json.h
+++ json.h
@@ -12,39 +12,43 @@
 #define DOU_QUO '\"'
 #define NONE 0
 
-typedef union{
-    int sym;
-
-    char * key;
-
-    int size;
-} Key;
-
-typedef union{
-    char * value;
-
-    struct JsonEntity * entity;
-
-    struct JsonEntity ** items;
-}Value;
-
 struct JsonEntity {
     int type;
-    Key desc;
-    Value data;
+
+    union{
+        struct Atom{
+            int sym;
+            char *value;
+        }atom;
+
+        struct Pair{
+            char *key;
+            struct JsonEntity *entity;
+        }pair;
+
+        struct Array{
+            int size;
+            struct JsonEntity **items;
+        }array;
+
+        struct Object{
+            int size;
+            struct JsonEntity **items;
+        }object;
+    }json;
 };
 
 #define CHILD_END ((struct JsonEntity *) NULL)
 
-#define type(json) ((json)->type)
-#define sym(json) ((json)->desc.sym)
-#define key(json) ((json)->desc.key)
-#define size(json) ((json)->desc.size)
-#define value(json) ((json)->data.value)
-#define entity(json) ((json)->data.entity)
-#define items(json) ((json)->data.items)
-#define item(json, i) (items(json)[i])
-#define inc(json) (++size(json))
+#define type(jsonEntity) ((jsonEntity)->type)
+#define sym(jsonEntity) ((jsonEntity)->json.atom.sym)
+#define value(jsonEntity) ((jsonEntity)->json.atom.value)
+#define key(jsonEntity) ((jsonEntity)->json.pair.key)
+#define entity(jsonEntity) ((jsonEntity)->json.pair.entity)
+#define size(jsonEntity) ((jsonEntity)->json.object.size)
+#define items(jsonEntity) ((jsonEntity)->json.object.items)
+#define item(jsonEntity, i) (items(jsonEntity)[i])
+#define inc(jsonEntity) (++size(jsonEntity))
 
 struct JsonEntity * add_Child(struct JsonEntity *, struct JsonEntity *);
 struct JsonEntity * add_Children(struct JsonEntity *, struct JsonEntity *, ...);
